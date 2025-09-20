2b17a34a485b975b1236845b8f478e5a14bf2175
Dilshan221 <dunith0773239462@gmail.com>
2025-09-20 20:38:40 +0530

    k

--- frontend/src/components/website/PublicShell.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/website/PublicShell.jsx
-import React from "react";
-// IMPORTANT: import the scoped CSS here (not in App.js)
-import "../../assets/css/style.css";
-
-export default function PublicShell({ children }) {
-  return <div className="public-site">{children}</div>;
-}
